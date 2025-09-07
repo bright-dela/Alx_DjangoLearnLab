a6611e8d10bf591a68be2f0f245bb223ac4d6464
bright-dela <brightdelasettor@gmail.com>
2025-09-07 18:32:03 +0000

    Refactor models.py and views.py for improved structure and readability

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,5 @@
-from django.contrib.auth.decorators import user_passes_test, permission_required
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
 from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
