449e32766a9a74930d677e8e35cc0d47663d8d77
bright-dela <brightdelasettor@gmail.com>
2025-09-07 17:47:46 +0000

    Refactor books_by_author and librarian_of_library functions for clarity

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -2,14 +2,13 @@ from relationship_app.models import Author, Book, Library, Librarian
 
 
 def books_by_author(author_name):
-    author = Author.objects.get(name=author_name)  
-    return Book.objects.filter(author=author)
+    author = Author.objects.get(name=author_name)   # explicitly get the author
+    return Book.objects.filter(author=author)       # then filter using the object
 
 def books_in_library(library_name):
     library = Library.objects.get(name=library_name)
     return library.books.all()
 
-
 def librarian_of_library(library_name):
     library = Library.objects.get(name=library_name)
-    return library.librarian
+    return Librarian.objects.get(library=library)   # explicit query with .get()
