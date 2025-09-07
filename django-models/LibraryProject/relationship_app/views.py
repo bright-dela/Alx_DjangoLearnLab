ea1f3cea0eb6bef7cc38c6d48ad66bb60ef666da
bright-dela <brightdelasettor@gmail.com>
2025-09-07 18:11:52 +0000

    Refactor views.py for improved structure and readability

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,12 +3,10 @@ from django.contrib.auth import login, authenticate, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-
-# Create your views here.
-
 from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 # Function-based view
 def list_books(request):
