de5d8346323294ef93159ccdaaf403ec217a5af1
bright-dela <brightdelasettor@gmail.com>
2025-09-07 18:27:13 +0000

    Refactor authentication views in urls.py and views.py for consistency and clarity

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -31,20 +31,6 @@ def register_view(request):
         form = UserCreationForm()
     return render(request, "relationship_app/register.html", {"form": form})
 
-def login_view(request):
-    if request.method == "POST":
-        form = AuthenticationForm(data=request.POST)
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            return redirect("list_books")
-    else:
-        form = AuthenticationForm()
-    return render(request, "relationship_app/login.html", {"form": form})
-
-def logout_view(request):
-    logout(request)
-    return render(request, "relationship_app/logout.html")
 
 def is_admin(user):
     return hasattr(user, "userprofile") and user.userprofile.role == "Admin"
